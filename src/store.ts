0e48a431acd3f357253288c2cf5d015f12bdbdfe
Lakshya Gupta <lakshya1176@gmail.com>
2026-03-12 20:18:36 +0530

    combineReducers to simplify code and typeof for state type

--- src/store.ts
+++ src/store.ts
@@ -1,28 +1,18 @@
-import { createStore, type AnyAction } from "redux";
-import { sadnessReducer, initialSadState, type sadState } from "./reducers/sadnessReducer";
-import { happinessReducer, initialHappyState, type happyState } from "./reducers/happinessReducer";
+import { createStore, combineReducers } from "redux";
+import { sadnessReducer } from "./reducers/sadnessReducer";
+import { happinessReducer } from "./reducers/happinessReducer";
 
 export type Moment = {
     intensity: number;
     when: Date;
 }
 
-export type State = {
-    sad: sadState;
-    happy: happyState;
-}
+const reducer = combineReducers({
+    sad: sadnessReducer,
+    happy: happinessReducer
+})
 
-const initialState: State = {
-    happy: initialHappyState,
-    sad: initialSadState
-}
-
-const reducer = (currentState: State = initialState, action: AnyAction): State => {
-    return {
-        sad: sadnessReducer(currentState.sad, action),
-        happy: happinessReducer(currentState.happy, action)
-    }
-}
+export type State = ReturnType<typeof reducer>
 
 const store = createStore(reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
