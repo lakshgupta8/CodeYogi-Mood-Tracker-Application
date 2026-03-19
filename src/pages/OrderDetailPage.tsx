9ae1814ecc188412b7e5e4f1fd6a03af0671a28a
Lakshya Gupta <lakshya1176@gmail.com>
2026-03-19 20:59:20 +0530

    styling order details page

--- src/pages/OrderDetailPage.tsx
+++ src/pages/OrderDetailPage.tsx
@@ -32,16 +32,19 @@ export const OrderDetailPage: FC = () => {
     return (
         <div>
             <Link to="/orders"><Button>Back</Button></Link>
-            <div>
-                <p className='font-medium text-lg'>Order Detail Page for order {orderid}</p>
+            <div className='bg-gray-200 m-2 p-2 rounded-md font-medium text-lg text-black'>
+                <p className='text-indigo-700 text-3xl font-bold mb-4'>Order Detail Page for order {orderid}</p>
                 <p>Order number: {order.id}</p>
                 <p>Total products: {order.totalProducts}</p>
                 <p>Total quantity: {order.totalQuantity}</p>
                 <p>Total Amount: {order.total}</p>
                 <p>Discounted total: {order.discountedTotal}</p>
-                <div>Products:
+                <div className='font-bold mt-4'>Products:
                     {products.map((p) => (
-                        <div key={p.id}>{p.title}</div>
+                        <div key={p.id} className='bg-white m-2 p-2 rounded-md shadow-sm flex justify-between'>
+                            <span>{p.title}</span>
+                            <span className='text-indigo-700'>Rs. {p.price}</span>
+                        </div>
                     ))}
                 </div>
             </div>
